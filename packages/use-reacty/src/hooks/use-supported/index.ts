319539511f497d724563a0164aa9a810e49cd51a
Xenos112 <acristyano22@gmail.com>
2025-06-03 23:25:06 +0000

    fix(useSupported): add jsdoc on top of the function

--- packages/use-reacty/src/hooks/use-supported/index.ts
+++ packages/use-reacty/src/hooks/use-supported/index.ts
@@ -1,4 +1,10 @@
-// eslint-disable-next-line react-hooks-extra/no-unnecessary-use-prefix
+/* eslint-disable react-hooks-extra/no-unnecessary-use-prefix */
+/**
+ * @name useSupported
+ * @description a hook to check if a feature is useable in the browser
+ * @param cb - a callback to check if the feature exists
+ * @returns boolean to indicate if it exist
+ */
 function useSupported(cb: () => unknown): boolean {
   return Boolean(cb())
 }
