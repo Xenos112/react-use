09abd6f6b5845646ddcc77c9b27a1fa661bb80fd
Xenos112 <acristyano22@gmail.com>
2025-06-05 09:07:59 +0000

    fix: add JSDoc comments for isIdle and lastActive properties in UseIdleReturnType interface

--- packages/use-reacty/src/hooks/use-idle/types.ts
+++ packages/use-reacty/src/hooks/use-idle/types.ts
@@ -1,4 +1,14 @@
 export interface UseIdleReturnType {
+  /**
+   * isIdle
+   * @description a boolean indicating is the user is in idle mode or not
+   * @default false
+   */
   isIdle: boolean
+  /**
+   * lastActive
+   * @description integer that got updated each second, when its > 0 it mean the user is idle
+   * @default -1
+   */
   lastActive: number
 }
