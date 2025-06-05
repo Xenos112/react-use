75074b3aac11ddb53a4e92806f716c223c3ecfa3
Xenos112 <acristyano22@gmail.com>
2025-06-05 09:14:58 +0000

    fix(useWindowScroll): better jsdoc for the return type

--- packages/use-reacty/src/hooks/use-window-scroll/types.ts
+++ packages/use-reacty/src/hooks/use-window-scroll/types.ts
@@ -1,7 +1,31 @@
 export interface WindowScroll {
+  /**
+   * scrollX
+   * @description function to navigate in the X axis
+   */
   scrollX: (value: number) => void
+
+  /**
+   * scrollY
+   * @description function to navigate in the Y axis
+   */
   scrollY: (value: number) => void
+
+  /**
+   * scroll
+   * @description function to navigate in the X and Y axis
+   */
   scroll: (x: number, y: number) => void
+
+  /**
+   * x
+   * @description current x position
+   */
   x: number
+
+  /**
+   * y
+   * @description current y positionA
+   */
   y: number
 }
